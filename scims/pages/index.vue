a718c3bfd661906dd86afc1aae3eac1e4808d3d5
Matthew Barton <mattbarton44@gmail.com>
2024-06-23 22:44:37 +0100

    chore: update

--- scims/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1> Index  </h1>
-    test
-  </div>
-</template>
-
-<script setup>
-</script>
--- tigers-templates/src/components/GameResult.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="">
-    GameResult
-  </div>
-</template>
-
-<script setup>
-
-</script>
